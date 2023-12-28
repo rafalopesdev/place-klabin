81276a38544dbd209da3ee23244a145a32182f02
Rafael Lopes <rafael.lopesdesigner@gmail.com>
2023-12-28 14:30:40 -0300

    estilo_css

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Praca from '../assets/praca.png';
+
+const About = () => {
+  return (
+    <div className="bg-[#002631] w-full h-full">
+      <div className="pt-8">
+        <img src={Praca} alt="" width={700} />
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,34 @@ import Banner from '../assets/banner.jpg';
 const Header = () => {
   return (
     <div className="w-full h-screen relative">
-      <div className="w-full h-16 bg-black absolute">
-        <div className="container m-auto flex justify-between">
-          <a href="" className="">
-            <img src={Logo} alt="Logo Place Kalbin" width={90} />
-          </a>
-          <nav className="mt-5 text-white">
-            <a href="">Home</a>
-            <a href="">Sobre Nós</a>
-            <a href="">Serviços</a>
-            <a href="">Contato</a>
+      <div className="w-full absolute">
+        <div className="w-full h-full bg-black border-solid border-b-2 border-[#BB3A02]">
+          <nav className="container h-16 flex justify-between m-auto ">
+            <a href="" className="">
+              <img src={Logo} alt="Logo Place Kalbin" width={90} />
+            </a>
+            <ul className="w-3/4 grid grid-cols-4 mt-3 mb-3 text-white items-center font-light uppercase">
+              <a href="" className="text-right">
+                Projeto
+              </a>
+              <a href="" className="text-right">
+                Apartamentos
+              </a>
+              <a href="" className="text-right">
+                Localização
+              </a>
+              <a href="" className="text-right">
+                Institucional
+              </a>
+            </ul>
           </nav>
         </div>
+        <div className="w-full bg-white/[.4] text-center p-3">
+          Uma praça inteira no seu jardim
+        </div>
       </div>
-      <img src={Banner} alt="" className="" />
+      <div className="w-full bg-black absolute bottom-0">footer</div>
+      <img src={Banner} alt="" className="h-screen object-cover" />
     </div>
   );
 };
